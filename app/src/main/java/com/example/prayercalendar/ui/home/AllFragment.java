7e2ffa2935d08d647e61a93461090860c5a3f5bd
James White <james.white@dydt.io>
2019-11-04 23:39:56 -0500

    Made a quick release

---
+++ app/src/main/java/com/example/prayercalendar/ui/home/AllFragment.java
@@ -0,0 +1,35 @@
+package com.example.prayercalendar.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.prayercalendar.R;
+
+public class AllFragment extends Fragment {
+
+    private AllViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(AllViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_all, container, false);
+        final TextView textView = root.findViewById(R.id.fab);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
